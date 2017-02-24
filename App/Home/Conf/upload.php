886ef081e7b119a9bf2843f013ce4cb72b00c9f1
dragon <dragonzhu1988@126.com>
2017-02-24 17:14:49 +0800

    new app

---
+++ App/Home/Conf/upload.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zdc
+ * Date: 17-2-24
+ * Time: 下午4:38
+ */
+
+return [
+    'UPLOAD_NAME' => 'upload_name',
+];
---
+++ App/Home/Controller/UserController.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zdc
+ * Date: 17-2-24
+ * Time: 下午4:57
+ */
+
+namespace Home\Controller;
+
+
+use Think\Controller;
+
+class UserController extends Controller
+{
+	public function index(){
+		echo "index";
+	}
+
+	public function edit(){
+		echo "edit";
+	}
+
+	public function login(){
+		echo "login";
+	}
+}
